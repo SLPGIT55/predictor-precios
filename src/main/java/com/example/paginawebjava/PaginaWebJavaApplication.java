16643700a26193f1c6f42d3c287f066bd9301b73
sandra lara <sandragithub@gmail.com>
2025-12-15 18:16:01 +0100

    Primer commit: Estructura base del proyecto Spring Boot con nombre de autor configurado

---
+++ src/main/java/com/example/paginawebjava/PaginaWebJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.paginawebjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginaWebJavaApplication.class, args);
+    }
+
+}
